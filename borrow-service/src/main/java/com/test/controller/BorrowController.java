cae0e92c622b651a42571eb05c20e357cfb11516
繁星 <765472804@qq.com>
2023-03-08 11:01:20 +0800

    sentinel服务熔断降级限流

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -1,21 +1,89 @@
 package com.test.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSONObject;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Collections;
 
 @RestController
 public class BorrowController {
 
     @Resource
     BorrowService service;
-
+    @SentinelResource("getBorrow")   //监控此方法，无论被谁执行都在监控范围内，这里给的value是自定义名称，这个注解可以加在任何方法上
     @RequestMapping("/borrow/{uid}")
     UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid){
         return service.getUserBorrowDetailByUid(uid);
     }
+    @RequestMapping("/blocked")
+    JSONObject blocked(){
+        JSONObject object = new JSONObject();
+        object.put("code", 403);
+        object.put("success", false);
+        object.put("massage", "您的请求频率过快，请稍后再试！");
+        return object;
+    }
+
+
+    @SentinelResource(value = "getBorrow2",blockHandler = "blocked")   //监控此方法，无论被谁执行都在监控范围内，这里给的value是自定义名称，这个注解可以加在任何方法上
+    @RequestMapping("/borrow2/{uid}")
+    UserBorrowDetail findUserBorrows2(@PathVariable("uid") int uid){
+        return service.getUserBorrowDetailByUid(uid);
+    }
+    //替代方案，注意参数和返回值需要保持一致，并且参数最后还需要额外添加一个BlockException
+    public UserBorrowDetail blocked(int uid, BlockException e) {
+        return new UserBorrowDetail(null, Collections.emptyList());
+    }
+
+
+    @RequestMapping("/test")
+    @SentinelResource(value = "test",
+            fallback = "except",    //fallback指定出现异常时的替代方案
+            blockHandler = "blocked",
+            exceptionsToIgnore = IOException.class)  //忽略那些异常，也就是说这些异常出现时不使用替代方案
+    String test(){
+        throw new RuntimeException("HelloWorld！");
+    }
+    //替代方法必须和原方法返回值和参数一致，最后可以添加一个Throwable作为参数接受异常
+    String except(Throwable t){
+        return t.getMessage();
+    }
+    //测试fallback和blockHandler的优先级
+    String blocked(BlockException e){
+        return "被限流了";
+    }
+
+
+    //热点限流
+    @RequestMapping("/test2")
+    @SentinelResource("test2")   //注意这里需要添加@SentinelResource才可以，用户资源名称就使用这里定义的资源名称
+    String test2(@RequestParam(value = "a", required = false) String a,
+                            @RequestParam(value = "b", required = false) String b,
+                            @RequestParam(value = "c",required = false) String c) {
+        return "请求成功！a = "+a+", b = "+b+", c = "+c;
+    }
+
+    //满调用熔断
+    @RequestMapping("test3")
+    String test3() throws InterruptedException {
+        Thread.sleep(1000);
+        return "你好";
+    }
+
+    //异常比例和异常数熔断
+    @RequestMapping("test4")
+    String test4() {
+        throw new RuntimeException("异常了");
+    }
+
+
 }
