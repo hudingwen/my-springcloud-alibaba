1b8462adb22f5047efdf83ac768e259988c4b336
繁星 <765472804@qq.com>
2023-03-09 10:09:16 +0800

    seata分布式事务相关sql

--- file/sql/springcloud.sql
+++ file/sql/springcloud.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2023-03-02 10:52:21
+Date: 2023-03-09 10:08:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,22 +23,23 @@ CREATE TABLE `db_book` (
   `bid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `desc` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `count` int(11) NOT NULL,
   PRIMARY KEY (`bid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of db_book
 -- ----------------------------
-INSERT INTO `db_book` VALUES ('1', 'JAVA入门', '了解并入门java的大门');
-INSERT INTO `db_book` VALUES ('2', 'JAVA基础', '了解各类语法和特性');
-INSERT INTO `db_book` VALUES ('3', 'JAVA高级', '了解编译原理');
+INSERT INTO `db_book` VALUES ('1', 'JAVA入门', '了解并入门java的大门', '2');
+INSERT INTO `db_book` VALUES ('2', 'JAVA基础', '了解各类语法和特性', '3');
+INSERT INTO `db_book` VALUES ('3', 'JAVA高级', '了解编译原理', '3');
 
 -- ----------------------------
 -- Table structure for db_borrow
 -- ----------------------------
 DROP TABLE IF EXISTS `db_borrow`;
 CREATE TABLE `db_borrow` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) DEFAULT NULL,
   `bid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -46,13 +47,12 @@ CREATE TABLE `db_borrow` (
   KEY `fk_bid` (`bid`),
   CONSTRAINT `fk_bid` FOREIGN KEY (`bid`) REFERENCES `db_book` (`bid`),
   CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `db_user` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of db_borrow
 -- ----------------------------
 INSERT INTO `db_borrow` VALUES ('1', '1', '1');
-INSERT INTO `db_borrow` VALUES ('2', '2', '3');
 
 -- ----------------------------
 -- Table structure for db_user
@@ -63,11 +63,35 @@ CREATE TABLE `db_user` (
   `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `age` int(11) NOT NULL,
   `sex` enum('男生','女生','未知') COLLATE utf8mb4_general_ci NOT NULL,
+  `book_count` int(11) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Records of db_user
 -- ----------------------------
-INSERT INTO `db_user` VALUES ('1', '小明', '18', '男生');
-INSERT INTO `db_user` VALUES ('2', '小红', '19', '女生');
+INSERT INTO `db_user` VALUES ('1', '小明', '18', '男生', '9');
+INSERT INTO `db_user` VALUES ('2', '小红', '19', '女生', '10');
+INSERT INTO `db_user` VALUES ('3', '小青', '20', '未知', '10');
+
+-- ----------------------------
+-- Table structure for undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of undo_log
+-- ----------------------------
